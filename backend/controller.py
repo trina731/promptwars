d08d8c73e420bda9732710bc0e087b37af1c2a83
trina731 <trinac0731@gmail.com>
2024-03-23 17:28:21 -0700

    add mistral api query

--- backend/controller.py
+++ backend/controller.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from mistral import query_mistral
 
 app = Flask(__name__)
+
+@app.route("/generate")
+def generate():
+    return query_mistral("This is a test query. Can you name a French painter?")
 
 @app.route("/")
 def hello_world():
---
+++ backend/mistral.py
@@ -0,0 +1,38 @@
+import os
+# from dotenv import load_dotenv
+import requests
+from flask import jsonify
+import json
+import asyncio
+
+# MISTRAL_API_KEY = os.getenv('MISTRAL_API_KEY')
+MISTRAL_API_KEY = 'TWfVrlX659GSTS9hcsgUcPZ8uNzfoQsg'
+
+def query_mistral(question):
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + MISTRAL_API_KEY,
+    }
+
+    json_data = {
+        'model': 'mistral-large-latest',
+        'messages': [
+            {
+                'role': 'user',
+                'content': question,
+            },
+        ],
+    }
+
+    # Send Mistral query
+    response = requests.post('https://api.mistral.ai/v1/chat/completions', headers=headers, json=json_data)
+
+    mistral_response = json.loads(response.text)["choices"]
+
+    if not mistral_response[0]["message"]:
+        print("ERROR: Mistral API did not return a response.")
+    else:
+        print(f"Received Mistral API response: {mistral_response}.")
+
+    return mistral_response[0]["message"]
